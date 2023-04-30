e2e5f4e6d3d18da98cea15d8c3d987047e6a7fa6
Wiktowin <vik.nagreb@gmail.com>
2023-05-01 00:15:25 +0200

    c

---
+++ calculator/complex.py
@@ -0,0 +1,16 @@
+import math
+
+impimort math
+
+def power(base, exponent):
+    return math.pow(base, exponent)
+
+def logarithm(number):
+    return math.log(number)
+
+import math
+
+def square_root(number):
+    return math.sqrt(number)
+def factorial(number):
+    return math.factorial(number)
